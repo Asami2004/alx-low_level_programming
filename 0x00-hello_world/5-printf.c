45589aff0543a7b583c59035d92b227b58b9bf79
Asami2004 <takhtoukhsami@gmail.com>
2023-06-15 16:17:47 -0700

    hi

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - display the text with proper grammar,
+ * hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii,
+ * Return: 0 if all goooooooooood
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
